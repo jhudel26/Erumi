114f4079644e067691a019acbfee5708e2b45c7f
jhudel26 <58135078+jhudel26@users.noreply.github.com>
2026-08-29 12:35:49 +0800

    Delete yorumi-cli-main directory

--- yorumi-cli-main/scratch.js
+++
@@ -1,6 +0,0 @@
-import { fetchAllAnimeStreams } from './src/allanime.js';
-
-(async () => {
-  const streams = await fetchAllAnimeStreams('Sousou no Frieren', 1, 'sub');
-  console.log(JSON.stringify(streams, null, 2));
-})();
